92f997ef4ae97ab859719d985f2b95229eb271c6
GameChanger77 <gamer4christ2004@gmail.com>
2023-12-09 21:15:29 -0600

    Added the other frontend code for the endpoints

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,21 +10,27 @@ function App() {
         {/* Navigation Bar */}
         <nav>
           <p>
-              <Link to="/get" className="link">GET</Link>
+              <Link to="/get" className="link">Get Product</Link>
               <Link to="/getid" className="link">Get by Id</Link>
+              <Link to="/createProduct" className="link">Create Product</Link>
+              <Link to="/updateProductid" className="link">Update Product</Link>
+              <Link to="/deleteProductid" className="link">Delete Product</Link>
+              <Link to="/about" className="link">About</Link>
           </p>
         </nav>
         {/* Routes */}
         <Routes>
           <Route path="/get" element={<GET />} />
           <Route path="/getid" element={<GETID />} />
+          <Route path="/createProduct" element={<CreateProduct />} />
+          <Route path="/updateProductid" element={<UpdateProduct />} />
+          <Route path="/deleteProductid" element={<DeleteProduct />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
-export default App;
 
 const GET = () => {
   const [product, setProduct] = useState([]);
@@ -52,9 +58,6 @@ const GET = () => {
   ));
   return (
     <div>
-      {/* -------------
-  Method GET all products
-  -----------------*/}
       <h1>Catalog of Products</h1>
       <div>
         <h3>Show all available Products.</h3>
@@ -93,9 +96,6 @@ const GETID = () => {
   return (
     <div>
       <h1>Show one Product by Id </h1>
-      {/* -------------
-  Method GET one product
-  -----------------*/}
       <div>
         <input
           type="text"
@@ -109,3 +109,107 @@ const GETID = () => {
     </div>
   );
 };
+
+const UpdateProduct = () => {
+  return (
+    <div>
+      <h1>Update Product</h1>
+      <h1>Update Product</h1>
+      <h1>Update Product</h1>
+    </div>
+  );
+};
+
+const DeleteProduct = () => {
+  return (
+    <div>
+      <h1>Delete Product</h1>
+      <h1>Delete Product</h1>
+      <h1>Delete Product</h1>
+    </div>
+  );
+}
+
+const CreateProduct = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    category: "",
+    price: "",
+    rating: "",
+    // Add other fields as needed
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your logic to send the form data to the backend API for product creation
+    console.log("Form data submitted:", formData);
+    // You can use fetch or a library like axios to send a POST request to your API
+  };
+
+  return (
+    <div>
+      <h1>Create Product</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={formData.title}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="category">Category:</label>
+        <input
+          type="text"
+          id="category"
+          name="category"
+          value={formData.category}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="price">Price:</label>
+        <input
+          type="text"
+          id="price"
+          name="price"
+          value={formData.price}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input
+          type="text"
+          id="rating"
+          name="rating"
+          value={formData.rating}
+          onChange={handleInputChange}
+        />
+
+        {/* Add other form fields as needed */}
+        
+        <button type="submit">Create Product</button>
+      </form>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1>InSert Student information here lol</h1>
+      <h1>InSert Student information here lol</h1>
+      <h1>InSert Student information here lol</h1>
+    </div>
+  )
+}
+
+export default App;
